bde971b1fd7f26419d9d0f4eace770ef45b45623
WickedSoulDK <37363619+Fjorten@users.noreply.github.com>
2024-06-14 09:19:10 +0200

    Update config.lua
    Added more models and rebalanced the amounts for my purpose..

--- config.lua
+++ config.lua
@@ -1,31 +1,33 @@
 Config = Config or {}
 
 Config.AmountToFind = math.random(1, 5) -- The range of how many items you can find in each dumpster/trash, can be a solid number too!
-Config.QBVersion == 'oldqb' -- Just the logic of how inventory itembox is handled! "oldqb" or "newqb"
+Config.QBVersion == 'newqb' -- Just the logic of how inventory itembox is handled! "oldqb" or "newqb"
 
 Config.Trashcans = { -- The models of the trashcans you want to be searchable!
     'prop_dumpster_02a',
+	'prop_dumpster_02b',
     'prop_dumpster_01a',
     'pop_cs_bin_02',
     'prop_bin_01a',
+	'prop_bin_07a'
 }
 
 Config.ItemsToFind = { -- The items that can be found in trashcans
     {
         item = 'phone', -- The ID/Name of the item from inventory
         price = 0, -- Just leave this at 0
-        amount = math.random(2, 4), -- Can be a solid number
+        amount = 1, -- Can be a solid number
         info = {}, -- If you want a specific data to be assigned for the item
         type = 'item', -- Just the category of the item
-        chance = 50 -- How high a chance it is to find!
+        chance = 1 -- How high a chance it is to find!
     },
     {
         item = 'tablet',
         price = 0,
-        amount = math.random(2, 4),
+        amount = 1,
         info = {},
         type = 'item',
-        chance = 50
+        chance = 1
     },
     {
         item = 'bandage',
@@ -33,7 +35,7 @@ Config.ItemsToFind = { -- The items that can be found in trashcans
         amount = math.random(2, 4),
         info = {},
         type = 'item',
-        chance = 50
+        chance = 5
     },
     {
         item = 'cryptostick',
@@ -41,14 +43,14 @@ Config.ItemsToFind = { -- The items that can be found in trashcans
         amount = 1,
         info = {},
         type = 'item',
-        chance = 50
+        chance = 1
     },
     {
         item = 'bread',
         price = 0,
-        amount = 1,
+        amount = math.random(2, 4),
         info = {},
         type = 'item',
-        chance = 100
+        chance = 33
     },
 }
